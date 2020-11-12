f95f07b621dc1d28c25edc6e6e5712badbd4b367
Alnair Gonzalez <a326669@uach.mx>
2020-11-12 11:16:10 -0700

    Acabado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Parrafo from "./components/Parrafo"
 import Contador from "./components/Contador"
 import User from "./components/User"
+import Foco from "./components/Foco"
 
 function App() {
   return (
     <div>
-      <h1>
-        hola mundo
-      </h1>
-      <p>lorem</p>
-      <Parrafo></Parrafo>
-      <Contador></Contador>
-      <User></User>
+      <table>
+        <tr>
+        <td><Foco lugar = "Sala"></Foco></td>
+        <td><Foco lugar = "Comedor"></Foco></td>
+        <td><Foco lugar = "Cocina"></Foco></td>
+        </tr>
+        <tr>
+        <td><Foco lugar = "Estancia"></Foco></td>
+        <td><Foco lugar = "Recamara Principal"></Foco></td>
+        <td><Foco lugar = "Recamara de los niños"></Foco></td>
+        </tr>
+      </table>
     </div>
   );
 }
